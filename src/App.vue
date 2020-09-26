d27f6e44c3a49a2249f409ea28fae31217692095
Togi Dashnyam <togi.berlin@gmail.com>
2020-09-26 12:22:32 +0200

    Get rid of boilerplate inside App.vue and add CSS Flexbox and MediaQuery.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body {
+  background: #F2F6FA;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@media screen and (min-width: 769px) {
+  html, body {
+    height: 100%;
   }
 }
 </style>

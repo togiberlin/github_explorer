2c58a8f8b745a4a383b20f1744977f47cf0aae6f
Togi Dashnyam <togi.berlin@gmail.com>
2020-09-26 20:09:50 +0200

    Fix Axios get req and typings

--- src/store/modules/repo/actions.ts
+++ src/store/modules/repo/actions.ts
@@ -3,13 +3,12 @@ import { ActionContext } from 'vuex';
 import { RepoItem, RepoMutationTypes, RepoState } from './types';
 
 const actions = {
-  async getRepoItems(context: ActionContext<RepoState, any>) {
+  async getRepoItems(context: ActionContext<RepoState, RepoState>) {
     try {
       const response: AxiosResponse<RepoItem[]> = await axios.get(
         'https://api.github.com/repositories', {
           headers: {
             Accept: 'application/vnd.github.v3+json',
-            'User-Agent': 'togiberlin/github_explorer',
           },
         },
       );

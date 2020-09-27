578b071e6c58f7905e94efc800e6531dc01e2aa1
Togi Dashnyam <togi.berlin@gmail.com>
2020-09-28 01:17:36 +0200

    Cleanup

--- src/components/repo/RepoListItem.vue
+++ src/components/repo/RepoListItem.vue
@@ -9,25 +9,7 @@
       Add to Favorites
     </span>
     <p>{{ repoItem.description }}</p>
-    <div>
-      <span class="has-text-primary has-text-weight-bold">
-        <i class="fa fa-user"></i> <a :href="repoItem.owner.url">{{ repoItem.owner.login }}</a>
-      </span>
-      <span class="has-text-primary has-text-weight-bold">
-        <i class="fa fa-code"></i> {{ (((repoItem || {}).details || {}).language) }}
-      </span>
-      <span class="has-text-primary has-text-weight-bold">
-        <i class="fa fa-star"></i> {{ (((repoItem || {}).details || {}).stargazers_count) }} Stars
-      </span>
-      <span class="has-text-primary has-text-weight-bold">
-        <i class="fa fa-binoculars">
-        </i> {{ (((repoItem || {}).details || {}).watchers_count) }} Watchers
-      </span>
-      <span class="has-text-primary has-text-weight-bold">
-        <i class="fa fa-share-alt"></i> {{ (((repoItem || {}).details || {}).forks_count) }} Forks
-      </span>
-      <RepoListItemDetails :repoItem="repoItem" />
-    </div>
+    <RepoListItemDetails :repoItem="repoItem" />
   </div>
 </template>
 
@@ -44,16 +26,8 @@ const RepoListItemComponent = Vue.extend({
       'addFavoriteItem',
     ]),
   },
-  watch: {
-    repoItem() {
-      this.$forceUpdate();
-    },
-  },
   components: {
     RepoListItemDetails,
-  },
-  updated() {
-    console.log(this.repoItem.details);
   },
 });
 
--- src/components/repo/RepoListItemDetails.vue
+++ src/components/repo/RepoListItemDetails.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Repo Details tbd</div>
+  <div>
+    <span class="has-text-primary">
+      <i class="fa fa-user"></i> <a :href="repoItem.owner.url">{{ repoItem.owner.login }}</a>
+    </span>
+    <span class="has-text-primary">
+      <i class="fa fa-code"></i> {{ (((repoItem || {}).details || {}).language) }}
+    </span>
+    <span class="has-text-primary">
+      <i class="fa fa-star"></i> {{ (((repoItem || {}).details || {}).stargazers_count) }} Stars
+    </span>
+    <span class="has-text-primary">
+      <i class="fa fa-binoculars">
+      </i> {{ (((repoItem || {}).details || {}).watchers_count) }} Watchers
+    </span>
+    <span class="has-text-primary">
+      <i class="fa fa-share-alt"></i> {{ (((repoItem || {}).details || {}).forks_count) }} Forks
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
